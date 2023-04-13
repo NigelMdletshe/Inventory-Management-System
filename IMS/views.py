b0791a406fc155332fabf87eb4c3605124b161c8
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-13 23:42:11 +0200

    initial commit

---
+++ IMS/views.py
@@ -0,0 +1,15 @@
+"""
+To render html web pages
+"""
+from django.http import HttpResponse 
+
+HTML_STRING = """
+<h1> Hello World </h1>
+ """ # This is a string
+
+def home(request):
+# Take in a request (Django sends request) Return
+#Html as a response (we pick the response)
+
+
+    return HttpResponse(HTML_STRING)
